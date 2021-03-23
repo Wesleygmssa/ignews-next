16bbe9b953e09308a92be6860d708d43f3864211
Wesley Guerra <guerra_ssa@hotmail.com>
2021-03-23 00:23:16 -0300

    Criando page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -7,7 +8,19 @@ export default function Home() {
         <title>ig.news</title>
       </Head>
 
-      <h1></h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world
+            <p>
+              Get access to all the publication <br />
+              <span>for $9.90 month</span>
+            </p>
+          </h1>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
