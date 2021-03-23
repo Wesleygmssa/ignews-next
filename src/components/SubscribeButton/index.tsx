3619e0709ebe5a7bf1e782c02eee266eb2e569fa
Wesley Guerra <guerra_ssa@hotmail.com>
2021-03-23 00:33:19 -0300

    Criando a interface

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,8 @@
+import styles from "./styles.module.scss";
+export function SubscribeButton() {
+  return (
+    <button className={styles.subscribeButton} type="button">
+      Subscribe bow
+    </button>
+  );
+}
