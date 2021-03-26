5f6a373e2ae53ae7f9a6703f043266305e7c0fe3
Wesley Guerra <guerra_ssa@hotmail.com>
2021-03-25 21:05:03 -0300

    Autenticação criada com sucesso

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,16 +1,28 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, useSession, signOut } from "next-auth/client";
 import styles from "./styles.module.scss";
+
 export function SignInButton() {
-  const isUserLoggedIn = false;
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  const [session] = useSession();
+  console.log(session);
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Wesley Guerra
+      {session.user.name}
       <FiX color="#b8c6f7" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sing in with GitHub
     </button>
